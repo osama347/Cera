f5a8907eb2045418a32c476f795fd5655ea90589
osama347 <b21f0800cs051@fecid.paf-iast.edu.pk>
2025-09-15 12:44:01 +0500

    maximize build chunk

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000, // in KB, set to 2MB for example
+  },
 })
