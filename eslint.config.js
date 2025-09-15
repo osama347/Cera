579d1646598c2ba713da3f1276e9bd7145e4e52f
osama347 <b21f0800cs051@fecid.paf-iast.edu.pk>
2025-09-15 12:39:30 +0500

    disabling lint

--- eslint.config.js
+++ eslint.config.js
@@ -21,13 +21,7 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      
     },
   },
 ]
